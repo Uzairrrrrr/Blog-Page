dde27d78f2909a8d229533b1578942ccab551da5
uzairrrrrr <uzairawan156@gmail.com>
2024-06-23 11:48:03 +0500

    set some css

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    latest_post = Post.objects.filter(latest=True).first()
+    other_posts = Post.objects.filter(latest=False).order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'latest_post': latest_post, 'other_posts': other_posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
